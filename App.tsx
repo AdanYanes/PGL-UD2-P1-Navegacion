fa2fe850aa04560d88abf5cca4e354d4f7d8934d
adansif <adan.yanesgonzalez@gmail.com>
2023-11-29 09:24:46 +0000

    Basic project setup

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+
+  return (
+    <View></View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
