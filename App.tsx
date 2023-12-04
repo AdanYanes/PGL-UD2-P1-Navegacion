e6d5a3617003fa286764d880024e12d1530506c9
adansif <adan.yanesgonzalez@gmail.com>
2023-12-04 17:55:35 +0000

    feat(welcome): Created welcome component

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native';
+import Welcome from './components/Welcome';
 
 export default function App() {
 
   return (
-    <View></View>
+    <View style={styles.appContainer}>
+      <Welcome></Welcome>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
   },
 });
