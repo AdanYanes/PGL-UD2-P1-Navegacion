55fe143cb67ce82508db130832646e2bff4d4b0b
adansif <adan.yanesgonzalez@gmail.com>
2024-01-24 17:16:13 +0000

    feat: Created a recordService

--- scenes/RecordAudio.tsx
+++ scenes/RecordAudio.tsx
@@ -2,105 +2,53 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
 import { Audio } from 'expo-av';
 import Ionicons from "@expo/vector-icons/Ionicons";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { RecordingFile, deleteRecordedFile, getDurationFormatted, playRecordedFile, startRecording, stopRecordedFile, stopRecording } from '../services/recordService';
 
-export type RecordingFile = {
-  duration: string;
-  uri: string;
-};
 
 const RecordAudio = () => {
-
+  
   const [recording, setRecording] = useState<Audio.Recording>();
   const [recordingList, setRecordingList] = useState<RecordingFile[]>([]);
+  const [playObject, setPlayObject] = useState<Audio.Sound | null>(null);
+  const [isPlayingList, setIsPlayingList] = useState(recordingList.map(() => false));
 
-  async function startRecording() {
+  const loadRecordingsFromStorage = async () => {
     try {
-      const permission = await Audio.requestPermissionsAsync();
-
-      if (permission.status === "granted") {
-        await Audio.setAudioModeAsync({
-          allowsRecordingIOS: true,
-          playsInSilentModeIOS: true,
-        });
+      const recordingsJson = await AsyncStorage.getItem('recordsList');
+      
+      let recordingsArray: RecordingFile[]
+      if (recordingsJson) {
+        const recordingsArray: RecordingFile[] = JSON.parse(recordingsJson);
+        setRecordingList(recordingsArray)
       }
-
-      console.log('Starting recording..');
-      const { recording } = await Audio.Recording.createAsync(Audio.RecordingOptionsPresets.HIGH_QUALITY);
-      setRecording(recording);
-      console.log('Recording started');
-    } catch (err) {
-      console.error('Failed to start recording', err);
+    } catch (error) {
+      console.log("Error loading recordings from AsyncStorage");
     }
-  }
+  };
 
-  async function stopRecording() {
-    console.log('Stopping recording..');
-    setRecording(undefined)
-    if (recording !== undefined) {
-      await recording.stopAndUnloadAsync();
-      await Audio.setAudioModeAsync(
-        {
-          allowsRecordingIOS: false,
-        }
-      );
-      const durationMillis: number = (await recording.getStatusAsync()).durationMillis;
-
-      let recordedFile: RecordingFile = {
-        duration: getDurationFormatted(durationMillis),
-        uri: recording.getURI() ?? "",
-      };
-
-      let updatedRecordings = [...recordingList];
-      updatedRecordings.push(recordedFile);
-
-      setRecordingList(updatedRecordings);
-      console.log('Recording stopped and stored at', recording.getURI());
-    }
-
-    function getDurationFormatted(millis: number) {
-      const minutes = millis / 1000 / 60;
-      const minutesDisplay = Math.floor(minutes);
-      const seconds = Math.round((minutes - minutesDisplay) * 60);
-      const secondsDisplay = seconds < 10 ? `0${seconds}` : seconds;
-      return `${minutesDisplay}:${secondsDisplay}`;
-    }
-  }
+  loadRecordingsFromStorage()
 
   function getRecordingLines() {
-    const [isPlayingList, setIsPlayingList] = useState(recordingList.map(() => false));
-
-    const playRecordFile = async (recordFile: RecordingFile, index: number): Promise<void> => {
-      const playObject = new Audio.Sound();
-      await playObject.loadAsync({ uri: recordFile.uri });
-      await playObject.playAsync();
-  
-      const newIsPlayingList = [...isPlayingList];
-      newIsPlayingList[index] = true;
-      setIsPlayingList(newIsPlayingList);
-    };
-
-    const deleteRecordFile = async (indexToRemove: number): Promise<void> =>{
-      const newRecordingList = recordingList.filter((_, index) => index !== indexToRemove);
-      setRecordingList(newRecordingList);
-    }
     return recordingList.map((recordingLine, index) => {
       return (
         <View>
           <View style={styles.recorder}>
-            <Text>Recording {index + 1} - {recordingLine.duration} </Text>
+            <Text>Recording {index + 1} - {getDurationFormatted(recordingLine.duration)} </Text>
             <TouchableOpacity style={{flexDirection: "row"}} >
               <Ionicons
                   name={"trash-outline"}
                   size={30}
                   color={'#F5B40C'}
-                  onPress={() => deleteRecordFile(index)}
+                  onPress={() => deleteRecordedFile(index, recordingList, setRecordingList)}
                 />
               <Ionicons
                 name={isPlayingList[index] ? 'stop-circle-outline' : 'play-circle-outline'}
                 size={30}
                 color={'#F5B40C'}
                 style={{paddingLeft: 20}}
-                onPress={() => playRecordFile(recordingLine, index)}
+                onPress={isPlayingList[index] ? () => stopRecordedFile(index, playObject, isPlayingList, setIsPlayingList) 
+                  : () => playRecordedFile(recordingLine, index, playObject, setPlayObject, setIsPlayingList, isPlayingList, recordingList)}
               />
             </TouchableOpacity>
           </View>
@@ -109,14 +57,13 @@ const RecordAudio = () => {
     })
   }
 
-
   return (
     <View>
       <View style={styles.recorder}>
         <Ionicons
           name="musical-notes-outline"
           size={40}
-          color={'#D00DF5'}
+          color={'#F5B40C'}
         />
         <Text>Tap mic icon to {recording ? "stop" : "start"} recording!</Text>
         {recording ?
@@ -125,14 +72,14 @@ const RecordAudio = () => {
               name="mic-off-outline"
               size={40}
               color={'#F5B40C'}
-              onPress={stopRecording}
+              onPress={() => stopRecording(setRecording, recording, recordingList, setRecordingList)}
             />
           ) : (
             <Ionicons
               name="mic-outline"
               size={40}
               color={'#F5B40C'}
-              onPress={startRecording}
+              onPress={() => startRecording(setRecording)}
             />
         )}
       </View>
